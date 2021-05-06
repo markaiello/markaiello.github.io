0e79efc16fab56c4b972aa1ee1686945fa048e7a
markaiello <59695132+markaiello@users.noreply.github.com>
2021-05-06 14:07:48 -0400

    Add files via upload

--- myScript3.js
+++ myScript3.js
@@ -5,7 +5,7 @@ window.onload=function(){
   // Get a reference to the database service
   
   var database = firebase.database();
-  console.log(database)
+ 
   
 var dauser;
 
@@ -88,6 +88,20 @@ var dauser;
     '3k4/7R/8/8/8/7Q/8/K7 w - - 1 1',
     '3k4/2R4Q/8/8/8/6q1/8/7K w - - 1 1',
     '1q6/8/8/8/8/2k5/8/K7 b - - 1 2',
+    
+    '8/8/8/8/8/3k4/1q3q2/3K4 b - - 1 2',
+    '6k1/5ppp/p7/P7/5b2/7P/1r3PP1/3R2K1 w - - 0 1',
+    'r1bqkb1r/pppp1ppp/2n2n2/3Q4/2B1P3/8/PB3PPP/RN2K1NR w KQkq - 0 1',
+    'rbb1k1nr/ppqn2pp/2p1p3/3p1pN1/2PP4/1PN1P3/P3BPPP/R1BQ1RK1 b kq - 0 1',
+    'r1b2rk1/pp3pp1/2nbpq1p/2pp2N1/3P3P/2P1P3/PPQ2PP1/RN2KB1R w KQ - 0 1',
+    'b4rk1/4bppp/p3qn2/8/2N5/2PrB1Q1/PP3P1P/2K1R1R1 w - - 0 1',
+    '6k1/5ppp/7q/Q7/5N2/6P1/2pN3P/4K2b w - - 0 1',
+    '1r2R3/8/2p2k1p/p5p1/Pp1n4/6Pq/QP3P2/4R1K1 b - - 0 1',
+    '2r2rk1/p4pp1/1p3n1p/4pN2/4P3/2q1P3/n1PRQ1PP/3K1R2 b - - 0 1',
+    '3r2rk/p5pp/1p4n1/3p2N1/2pP4/2P1R3/qPBQ1PPP/6K1 w - - 0 1',
+    '8/8/8/8/6N1/7b/4RB1P/5k1K b - - 0 1'
+
+
     ]
 
 
@@ -110,7 +124,6 @@ var dauser;
     dbRef.ref('users/' + dauser.uid + '/score').get().then((snapshot) => {
       if (snapshot.exists()) {
         const x = snapshot.val();
-        console.log(x)
 
         var y = document.getElementById("score") 
         y.innerText =''
@@ -177,11 +190,10 @@ var dauser;
     
       // checkmate?
       if (game.in_checkmate()) {
-        status = 'Game over, ' + moveColor + ' is in checkmate.'
+        status = 'Checkmate!'
 
        
         if(dauser !== undefined){
-          console.log(dauser)
         const dbRef = firebase.database();
         dbRef.ref('users/' + dauser.uid + '/score').get().then((snapshot) => {
           if (snapshot.exists()) {
@@ -199,7 +211,7 @@ var dauser;
 
       }
       else{
-          status = "That's not checkmate, try again?"
+          status = "That's not it, try again?"
       }
     
     
@@ -233,7 +245,8 @@ var dauser;
    function setupNewPuzzle(){
     movecounter = 0;
 
-    var x = Math.floor(Math.random() * 6)
+    var x = Math.floor(Math.random() * 17)
+ 
     
     
     
